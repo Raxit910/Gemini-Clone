cc802bb45d43381204ed3d8d910516bf9b35e65f
Raxit Sheladiya <raxitsheladiya@gmail.com>
2024-10-05 13:06:06 +0530

    Profile Updated

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -13,7 +13,7 @@ import message_icon from './message_icon.png'
 import code_icon from './code_icon.png'
 import send_icon from './send_icon.png'
 import gemini_icon from './gemini_icon.png'
-import dummy_icon from './dummy.png'
+import dummy_icon from './R-Letter.png'
 
 export const assets = {
     history_icon,
